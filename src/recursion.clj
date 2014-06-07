88be801ddf86a27660d51dfd702b94047b0b2f86
ypsilon.takai <ypsilon.takai@gmail.com>
2014-06-07 21:24:05 +0900

    under construction

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,34 +1,72 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (next coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (first coll)
+       (empty? (next coll))))
 
 (defn my-last [coll]
-  :-)
+  (cond (empty? coll) nil
+        (singleton? coll) (first coll)
+        :t (my-last (next coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond (empty? a-seq) nil
+        (singleton? a-seq) (first a-seq)
+        :t (let [third-and-over (next (next a-seq))]
+             (max-element (cons (max (first a-seq) (second a-seq))
+                                third-and-over)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (cond (empty? a-seq) nil
+        (singleton? a-seq) (first a-seq)
+        :t (let [third-and-over (next (next a-seq))]
+             (longest-sequence (cons (seq-max (first a-seq) (second a-seq))
+                                     third-and-over)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (cond (empty? a-seq)
+        ,,'()
+        (pred? (first a-seq))
+        ,,(cons (first a-seq)
+                (my-filter pred? (next a-seq)))
+        :t
+        ,,(my-filter pred? (next a-seq))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond (empty? a-seq)
+        ,,false
+        (== elem (first a-seq))
+        ,,true
+        :t
+        ,,(sequence-contains? elem (next a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond (empty? a-seq)
+        ,,'()
+        (pred? (first a-seq))
+        ,,(cons (first a-seq)
+                (my-take-while pred? (next a-seq)))
+        :t
+        ,,'()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond (empty? a-seq)
+        ,,'()
+        (pred? (first a-seq))
+        ,,(my-drop-while pred? (next a-seq))
+        :t
+        ,,a-seq))
 
 (defn seq= [a-seq b-seq]
   :-)
